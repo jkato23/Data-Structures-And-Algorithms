8be16c1fe28c75acfb41f2658841a118262dcc3b
jkato23 <jgmkato@gmail.com>
2025-03-15 21:17:44 -0700

    Create hash_tables.py

---
+++ hash_tables.py
@@ -0,0 +1,12 @@
+city_map = {"Canada": ["Calgary", "Vancouver", "Toronto"],
+            "USA": ["New York City", "Austin", "Seattle"],
+            "England": ["London", "Manchester"]
+            }
+
+print(city_map.keys())
+print(city_map.values())
+print(city_map.items())
+
+print(city_map["Canada"])
+print(city_map["USA"])
+print(city_map["England"])
